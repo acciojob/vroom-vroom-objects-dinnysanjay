443660a7f80750cc9aa6f99987f025853c13a69e
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-21 11:57:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function Car(make, model) {
 }
 car.prototype.getMakeModel=function(){
 	return `${this.make} ${this.model}`;
-}
+};
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
 	this.topSpeed=topSpeed;
 }
-SportsCar.prototype=object.create(Car.prototype);
+SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
 SportsCar.prototype.getTopSpeed=function(){
 	return this.topSpeed;
